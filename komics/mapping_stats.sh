7a804e05848c6225db54a4c654ed1141a4f77bc7
Frederik Van den Broeck <u0069739@gbw-l-m0667.local>
2021-10-06 16:27:40 +0200

    updated mapping_stats

--- komics/mapping_stats.sh
+++ komics/mapping_stats.sh
@@ -15,24 +15,28 @@ out=$(echo $samfile | sed 's/\.sam//g')
 
 Nreads=$(samtools view -c $samfile)
 NMreads=$(samtools view -c -F 4 $samfile)
+## added this:
+NMPreads=$(samtools view $samfile | awk '$2==81 || $2==83 || $2==161 || $2==163 || $2 == 97 || $2 == 99 || $2 == 145 || $2 == 147 { tot ++ $2 } END { print +tot}')
 NMQ20reads=$(samtools view -c -F 4 -q 20 $samfile)
 
 NCSBreads=$(samtools view $samfile | egrep -c 'GGGGTTGGTGT|ACACCAACCCC|GGGGTTGATGT|ACATCAACCCC')
 NCSBMreads=$(samtools view -F 4 $samfile | egrep -c 'GGGGTTGGTGT|ACACCAACCCC|GGGGTTGATGT|ACATCAACCCC')
-NCSBMQ20reads=$(samtools view -F 4 -q 20 $samfile | egrep -c 'GGGGTTGGTGT|ACACCAACCCC|GGGGTTGATGT|ACATCAACCCC')
 
-echo "Number of reads:" $Nreads >> $out.mapping.stats.txt
-echo "Number of mapped reads:" $NMreads >> $out.mapping.stats.txt
-echo "Number of reads with mapping quality >= 20:" $NMQ20reads >> $out.mapping.stats.txt
-echo "Number of CSB3-containing reads:" $NCSBreads >> $out.mapping.stats.txt
-echo "Number of mapped CSB3-containing reads:" $NCSBMreads >> $out.mapping.stats.txt
-echo "Number of CSB3-containing reads with mapping quality >= 20:" $NCSBMQ20reads >> $out.mapping.stats.txt
+## changed high quality to perfect alignments:
+NCSBperfectalignments=$(samtools view $samfile | egrep 'GGGGTTGGTGT|ACACCAACCCC|GGGGTTGATGT|ACATCAACCCC' | awk '{print $6}' | grep -cvE '[S/D/I/*]')
 
+echo "N_R:" $Nreads > $out.mapping.stats.txt # changed '>' to '>>'
+echo "N_MR:" $NMreads >> $out.mapping.stats.txt
+echo "N_MR_PP:" $NMPreads >> $out.mapping.stats.txt
+echo "N_MR_HQ:" $NMQ20reads >> $out.mapping.stats.txt
+echo "N_CSB3-R:" $NCSBreads >> $out.mapping.stats.txt
+echo "N_CSB3-R_M:" $NCSBMreads >> $out.mapping.stats.txt
+echo "N_CSB3-R_PA:" $NCSBperfectalignments >> $out.mapping.stats.txt
 
 # Calculating read depths
-samtools sort $samfile > $samfile.sorted.sam
-samtools depth -a $samfile.sorted.sam > $samfile\.depth
-MinicircleContigs=$(awk '{print $1}' $samfile\.depth | sort | uniq)
+samtools sort $samfile > $out.sorted.sam
+samtools depth -aa -d 0 $out.sorted.sam > $out\.depth ## changed 'a' to 'aa', changed '>' to '>>'
+MinicircleContigs=$(awk '{print $1}' $out\.depth | sort | uniq)
 
 calcs () {
 sort -n | awk '
@@ -57,10 +61,10 @@ sort -n | awk '
 '
 }
 
-echo "CONTIG" "AVERAGE.DEPTH" "MEDIAN.DEPTH" "MIN.DEPTH" "MAX.DEPTH ">> $out.depth.stats.txt
+echo "CONTIG" "AVERAGE.DEPTH" "MEDIAN.DEPTH" "MIN.DEPTH" "MAX.DEPTH " > $out.depth.stats.txt # changed '>' to '>>'
 
 for mini in $MinicircleContigs
-	do echo $mini $(grep "$mini" $samfile\.depth | awk '{print $3}' | calcs)  >> $out.depth.stats.txt
+	do echo $mini $(grep "$mini" $out\.depth | awk '{print $3}' | calcs)  >> $out.depth.stats.txt
 done
 
 fi
